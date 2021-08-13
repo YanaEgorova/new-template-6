5ab3695149b969f52249ea53dc4eab97e3afcc8d
YanaEgorova <yanaegorova69.3@gmail.com>
2021-08-14 02:52:39 +0300

    +

---
+++ js/data/products.js
@@ -0,0 +1,337 @@
+export const products = [
+    {
+        id: 'product-1',
+        name: 'Reusable Silicone Baking Cups',
+        price: 5.95,
+        image: './img/img1.png',
+        description: [
+            {
+                text: '12-pack of reusable baking cups; made of food-grade silicone; ideal for baking, lunch/snack cups, and more.',
+            },
+            {
+                text: 'Cost-saving alternative to disposable paper cups; works with any standard muffin pan.',
+            },
+            {
+                text: 'Effortless food release—no greasing or cooking spray needed; stain and odor resistant.',
+            },
+            {
+                text: 'Freezer-, microwave-, and oven-safe (up to 428 degrees F); dishwasher-safe for quick cleanup.',
+            },
+            {
+                text: 'Measures 3.5 cm high (1.38 inches); includes 2 of each color: pink, orange, yellow, green, blue, and purple.',
+            },
+        ],
+        images: ['./img/img1-1.png', './img/img1-2.png', './img/img1-3.png'],
+    },
+    {
+        id: 'product-2',
+        name: 'Car DC Charger',
+        price: 19.99,
+        image: './img/img2.png',
+        description: [
+            {
+                text: 'IN THE BOX: 6-foot car charging cable',
+            },
+            {
+                text: 'EASY TO USE: Plugs directly into a car’s DC power source (aka cigarette lighter socket); output: 5V / 3.0A',
+            },
+            {
+                text: 'USB-C CONNECTOR: Plugs into the USB port of a Switch device',
+            },
+            {
+                text: 'DURABLE DESIGN: Cable and connector provide reliable strength',
+            },
+        ],
+        images: ['./img/img2-1.png', './img/img2-2.png'],
+    },
+    {
+        id: 'product-3',
+        name: 'Non-Slip Clothes Suit Hangers',
+        price: 24.99,
+        discount: 14.99,
+        image: './img/img3.png',
+        description: [
+            {
+                text: 'Velvet surface and notched shoulders to prevent slipping.',
+            },
+            {
+                text: 'Ultra slim profile to maximize space in your closet.',
+            },
+            {
+                text: 'Sturdy design can hold up to 10 pounds.',
+            },
+            {
+                text: 'Ships in Certified Frustration-Free Packaging.',
+            },
+            {
+                text: 'Product Dimensions: 17.4 x 0.2 x 9.2 inches (LxWxH) ; 2.08 ounces.',
+            },
+        ],
+        images: ['./img/img3-1.png', './img/img3-2.png', './img/img3-3.png', './img/img3-4.jpg'],
+    },
+    {
+        id: 'product-4',
+        name: 'Resistance Loop Exercise Bands',
+        price: 35.32,
+        discount: 24.65,
+        image: './img/img4.png',
+        description: [
+            {
+                title: 'High end exercise bands',
+                text: 'Our 12″ by 2″ heavy duty loop resistance bands come in 5 varying resistance levels. This makes them perfect whether you are just starting to workout or a seasoned workout warrior. Our extra light and light bands are great for beginners, while our medium, heavy and extra heavy exercise bands are targeted for more intermediate and advanced strength training.',
+            },
+            {
+                title: 'Great with any workout',
+                text: 'This resistance band set can be integrated seamlessly with various popular workout program. Or use them for general exercise, stretching, strength training, power weight programs. The included carry bag makes it easy to take your bands with you and do any workout away from home or your home gym.',
+            },
+            {
+                title: 'Multiple uses',
+                text: 'While these resistance bands are often used for sports and fitness, physical therapists love these physical therapy bands (rehab bands) to help them rehabilitate their patients. Our stretch bands work for people suffering from leg, knee and back injuries. They are also perfect for use by women after pregnancy and birth to keep their bodies in shape.',
+            },
+            {
+                title: 'Superior quality',
+                text: 'All of our exercise resistance bands are thoroughly tested before we ship them out to you. This ensures your bands are easy on skin and will provide you with a worry free experience. The instruction booklet includes dozens of different illustrated exercises that demonstrate how to use our resistance bands for legs, arms, back, shoulders, ankles, hips and stomach.',
+            },
+            {
+                title: 'What you get',
+                text: 'Five exercise bands with color-coded resistance levels, portable travel carry bag and instruction guide.',
+            },
+        ],
+        images: ['./img/img4-1.png', './img/img4-2.png', './img/img4-3.png'],
+    },
+    {
+        id: 'product-5',
+        name: 'Ultrasonic Aromatherapy Essential Oil Diffuser',
+        price: 39.99,
+        discount: 39.97,
+        image: './img/img5.png',
+        description: [
+            {
+                text: 'Ultrasonic multi-functional aromatherapy diffuser with 300ml water capacity.',
+            },
+            {
+                text: 'Distributes evaporated essential oils into the air for creating a lightly scented, relaxing mood.',
+            },
+            {
+                text: '7 colored night light options for ambiance; digital display clock with alarm; built-in bluetooth speaker.',
+            },
+            {
+                text: '1, 3, and 5 hour timer modes; whisper quiet operation.',
+            },
+            {
+                text: 'Automatically shuts off once water tank is empty.',
+            },
+            {
+                text: 'Modern cube design with wood finish base.',
+            },
+        ],
+        images: ['./img/img5-1.png', './img/img5-2.jpg'],
+    },
+    {
+        id: 'product-6',
+        name: 'Window Curtains',
+        price: 43.98,
+        discount: 55.97,
+        image: './img/img6.png',
+        description: [
+            {
+                text: '100% Polyester',
+            },
+            {
+                text: '2-pack of 100% blackout curtain panels for blocking sunlight, UV rays, reducing outside noise, and ensuring total privacy in any room.',
+            },
+            {
+                text: 'Creates energy-saving insulating barrier against heat and cold, keeping room cooler in the summer and warmer in the winter.',
+            },
+            {
+                text: 'Rod pocket fit standard or decorative rods; includes 2 Velcro ties for pulling back the curtains.',
+            },
+            {
+                text: 'Constructed with innovative triple-weave fabric with thick blackout coating on the backside; machine washable.',
+            },
+            {
+                text: 'Dimensions: 52 x 84 inches per panel (Note: wrinkles from packing will be mitigated after hanging or ironing).',
+            },
+        ],
+        images: ['./img/img6-1.png', './img/img6-2.png', './img/img6-3.png', './img/img6-4.png'],
+    },
+    {
+        id: 'product-7',
+        name: 'LED String Lights',
+        price: 49.95,
+        discount: 69.99,
+        image: './img/img7.png',
+        description: [
+            {
+                text: '48-foot commercial grade outdoor string lights; UL listed with 1 bulb every 3 feet.',
+            },
+            {
+                text: 'With a life of 15,000 hours, the bulb will last over 13.7 years (based on 3 hours of use each day).',
+            },
+            {
+                text: 'With a correlated color temperature of 2700 kelvin, the string lights provide a soft white light, creating a warm ambiance for any patio, pergola or outdoor setting.',
+            },
+            {
+                text: 'This LED string light uses only 16 watts of energy (1 watt per bulb).',
+            },
+            {
+                text: 'This string light costs only $1.93 per year to operate (based on 3 hours/day, 11 cents/kWh) making it an economical alternative to incandescent bulbs.',
+            },
+            {
+                text: 'Durable, black cord.',
+            },
+        ],
+        images: ['./img/img7-1.png', './img/img7-2.png', './img/img7-3.png', './img/img7-4.png'],
+    },
+    {
+        id: 'product-8',
+        name: 'Trampoline for kids',
+        price: 69.99,
+        discount: 69.94,
+        image: './img/img8.png',
+        description: [
+            {
+                text: 'Perfect trampoline for toddlers to burn off energy.',
+            },
+            {
+                text: 'Features large jumping surface and handle bar for stability.',
+            },
+            {
+                text: 'Plastic and metal combination.',
+            },
+            {
+                text: 'Trampoline is for indoor use only. Age- 3 to 6 years. Ground to top of handle bar- 34.00 inch H. Jumping surface to top of handle bar- 26.25 inch H',
+            },
+        ],
+        images: ['./img/img8-1.png', './img/img8-2.png'],
+    },
+    {
+        id: 'product-9',
+        name: '',
+        price: 79.99,
+        discount: 79.93,
+        image: './img/img9.png',
+        description: [
+            {
+                title: 'Three speed belt-driven turntable',
+                text: 'This 3-speed (33 1/3, 45, 78 rpm) suitcase record player features UPGRADED PREMIUM SOUND QUALITY and sits on sound isolating feet that prevent vibration. It is perfect for your living room, bedroom or office.',
+            },
+            {
+                title: 'Take your tunes anywhere',
+                text: 'Housed in a vintage suitcase with an easy carry handle,  blend of retro and contemporary design give you the ultimate flexibility to listen to music where you want and how you want – a great choice for vinyl lovers.',
+            },
+            {
+                title: 'Expanded connection options',
+                text: 'Stream smartphone audio through the turntable’s built-in Bluetooth speakers. Easily connect external speakers via the stereo RCA outputs, or use the Line input for non-Bluetooth devices like a CD player. For personal listening, connect your headphones to the headphone jack.',
+            },
+            {
+                title: 'No stereo system or extra equipment required',
+                text: 'Get it up and running in minutes. Retro looks combined with the convenience of modern technology makes this affordable record player ideal for beginners & vintage enthusiasts.',
+            },
+            {
+                title: 'All the controls you need',
+                text: 'Features an Input select knob, Power/Volume Knob & an Auto stop switch that stops spinning records once the record is finished playing.',
+            },
+            {
+                title: 'A record player that suits your personality',
+                text: 'Combined with unparalleled sound quality and exquisite craftsmanship, and a wide range of styles, patterns and colors to choose from, get ready to create lifelong music memories.',
+            },
+        ],
+        images: ['./img/img9-1.png', './img/img9-2.jpg', './img/img9-3.png', './img/img9-4.jpg', './img/img9-5.jpg'],
+    },
+    {
+        id: 'product-10',
+        name: 'Kayak Inflatable Set with Aluminum Oars',
+        price: 84.41,
+        discount: 95.50,
+        image: './img/img10.png',
+        description: [
+            {
+                text: 'Nimble, durable kayak is made of durable welded material with eye catching graphics for added safety on the lake or slow moving river.',
+            },
+            {
+                text: 'Cockpit is designed for comfort and maximized space, and inflatable I beam floors add stability. Removable skeg provides exceptional directional movement.',
+            },
+            {
+                text: 'Cargo net to store extra gear, and grab line on both ends of kayak; Inflatable seat with backrest.',
+            },
+            {
+                text: 'Comes with 84 inch aluminum oar, repair patch and Hi output manual hand pump; Rugged vinyl construction.',
+            },
+            {
+                text: 'Measures 30 by 15 by 108 inches (W x H x D), with 27.2 pound weight and 220 pound maximum capacity.',
+            },
+        ],
+        images: ['./img/img10-1.png', './img/img10-2.jpg', './img/img10-3.png', './img/img10-4.jpg'],
+    },
+    {
+        id: 'product-11',
+        name: 'Paper Towel Dispenser',
+        price: 89.99,
+        discount: 109.91,
+        image: './img/img11.png',
+        description: [
+            {
+                title: 'Easy to Maintain',
+                text: 'High-capacity 1150-feet towel rolls last longer, requiring less maintenance and minimizing refill frequency.',
+            },
+            {
+                title: 'Affordable quality',
+                text: 'One-at-a-time dispensing helps prevent waste and helps prevent germ transmission.',
+            },
+            {
+                text: 'Paper hand towels are EPA Compliant for recycled fiber, thus a more environmentally friendly towel option.',
+            },
+        ],
+        images: ['./img/img11-1.png', './img/img11-2.png', './img/img11-3.png'],
+    },
+    {
+        id: 'product-12',
+        name: 'Storage/Organization, 5 drawer',
+        price: 109.00,
+        discount: 110.89,
+        image: './img/img12.png',
+        description: [
+            {
+                text: '5 drawers cabinet use as a dresser or just storage cabinet.',
+            },
+            {
+                text: 'Removable casters for stationary or mobility',
+            },
+            {
+                text: 'Assembly required.',
+            },
+        ],
+        images: ['./img/img12-1.png', './img/img12-2.png', './img/img12-3.png', './img/img12-4.png', './img/img12-5.png', './img/img12-6.png', './img/img12-7.jpg'],
+    },
+    {
+        id: 'product-13',
+        name: '',
+        price: 119.00,
+        discount: 120.88,
+        image: './img/img13.png',
+        description: [
+            {
+                title: 'Ultra-slim metal body',
+                text: 'Watch is equipped with pure black 3D curved bezel-less glass and polished stainless-steel back perfectly fused together, making the watch sleek and ultra-slim. With only 9 mm thickness, it\'s exceptionally comfortable to wear day and night.',
+            },
+            {
+                title: 'Blood oxygen saturation measurement',
+                text: 'SpO2 is an important indicator of health, especially in high-altitude and low-oxygen environments or while performing high-intensity sports. With this watch, you can measure your current blood-oxygen saturation instantly from your wrist.',
+            },
+            {
+                title: 'Sleep quality monitoring',
+                text: 'Monitor light sleep, deep sleep, rapid eye movement (REM), and awake time. It also provides you a score on sleep quality and breathing quality. It even monitors your 20-minute nap during the day.',
+            },
+            {
+                title: '7-day battery life',
+                text: 'Watch endures for up to 7 days in typical usages mode for a single full charge, which free you from the daily recharge.',
+            },
+            {
+                title: 'Notifications and alarms',
+                text: 'You will also receive notifications when your heart rate exceeds the recommended limit. Watch receives notifications for incoming calls, emails, messages and other applications without having the phone; sedentary reminders if you\'ve been sitting too long.',
+            },
+        ],
+        images: ['./img/img13-1.png', './img/img13-2.png'],
+    },
+]
